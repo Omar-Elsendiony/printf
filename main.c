6c3506be822303275425ef4ad6d68403212ccaa7
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-20 02:52:46 +0300

    testing and refinement

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 
 int main(void)
 {
+    _printf("%!\n");
     /*int c = _printf("12345%");
     printf("\n%d\n", c);
     _printf("%S", "hello\nworld\t");
     printf("%05d\n", -26); */
-
+    /*
     _printf("%+-05d\n", 26);
      printf("%+-5d\n", 26);
 
@@ -15,7 +16,7 @@ int main(void)
     printf("%05d\n", 26);
 
     _printf("%0+5d\n", 26);
-     printf("%0+5d\n", 26);
+     printf("%0+5d\n", 26);*/
     
     return 0;
 }
