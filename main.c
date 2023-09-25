41fbf6d3334c88533b32ce152843b1300ee7613c
MohammedAmer <loard.amer.2010@gmail.com>
2023-09-25 07:24:08 +0300

    testing hope this work

--- main.c
+++ main.c
@@ -1,20 +1,8 @@
 #include "main.h"
-#include <stdio.h>
+
 
 int main(void)
 {
-	int len, len2;
-
-	len = _printf("%");
-	len2 = printf("%");
-	fflush(stdout);
-	if (len != len2)
-	{
-        printf("%d\n", len);
-        printf("%d\n", len2);
-		printf("Lengths differ.\n");
-		fflush(stdout);
-		return (1);
-	}
+	
 	return (0);
 }
